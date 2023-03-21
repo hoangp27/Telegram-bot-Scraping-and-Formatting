2dc32265968518043688fe7d702ef76a5943d62d
Hoang Pham <63693369+hoangp27@users.noreply.github.com>
2023-03-21 02:36:36 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+
+import os
+import telebot
+#import requests
+#from bs4 import BeautifulSoup
+import keep_alive
+import pandas as pd
+
+keep_alive.keep_alive()
+
+##Web Scraping coinlist
+#URL = "https://www.binance.com/en/futures/trading-rules/perpetual"
+#page = requests.get(URL)
+#soup = BeautifulSoup(page.content,"html.parser")
+#results= soup.find(id="__APP")
+#coinPairList = soup.find_all("td", class_="rc-table-cell rc-table-cell-fix-left rc-table-cell-fix-left-last")
+
+
+CoinPairList = pd.read_csv("coinlist.csv")
+
+NumberList = []
+CoinList =CoinPairList.TradingPair.to_list()
+
+
+for x in range(0, len(CoinList)):
+  CoinList[x] = "#" + CoinList[x]
+  #CoinList.append(coinPair)
+
+
+
+print(CoinList)
+
+my_secret = os.environ['API']
+
+bot = telebot.TeleBot(my_secret)
+
+
+
+def check_float(potential_float):
+  try:
+    float(potential_float)
+  except ValueError:
+    return False
+  else:
+    return True
+
+def target_detect (message): 
+  message_element = message.text.split()
+  for text in message_element:
+    #print(text)
+    if check_float(text)== True:
+      NumberList.append(text)
+  NumberList.sort(key = float)
+  print(NumberList)
+ 
+  
+def coin_detect (message):
+  message_element = message.text.split()
+  #request = message_element[1].replace("/","")
+  request = message_element[0]
+  #print(request)
+  for coin in CoinList:
+    if request == coin:
+      target_detect(message)
+      return True
+  return False
+
+@bot.message_handler(func= coin_detect)
+def send_signal(message):
+  if len(NumberList) >= 7:
+    request = message.text.split()[0]
+    signal1 = ""
+    signal2 =""
+    signal1 = "signal buy " + request + "\n\n" + "❇️ Buy Price: "+ NumberList[1] + "\n" 
+    signal2 = "signal buy " + request + "\n\n" + "❇️ Buy Price: "+ NumberList[2] + "\n" 
+    for x in range(3,len(NumberList)):
+      signal1 = signal1 + "\n☑️ Target " + str(x-2) + ": " + NumberList[x]
+      signal2 = signal2 + "\n☑️ Target " + str(x-2) + ": " + NumberList[x]
+    signal1 = signal1 + "\n\n⛔ Stop Loss: " + NumberList[0]
+    signal2 = signal2 + "\n\n⛔ Stop Loss: " + NumberList[0]
+    bot.send_message(message.chat.id, signal1) 
+    bot.send_message(message.chat.id, signal2) 
+    NumberList.clear()
+
+#@bot.message_handler(func= coin_detect)
+#def send_signal(message):
+  #if len(NumberList) == 6:
+    #request = message.text.split()[0]
+    #response =""
+    #response = "signal buy " + request + "\n\n" + "❇️ Buy Price: "+ NumberList[1] + "\n\n☑️ Target 1: " + NumberList[2] + "\n☑️ Target 2: " + NumberList[3] + "\n☑️ Target 3: " + NumberList[4] + "\n☑️ Target 4: " + NumberList[5] + "\n\n⛔ Stop Loss: " + NumberList[0]
+  
+    #bot.send_message(message.chat.id, response)
+
+
+bot.polling()
